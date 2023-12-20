329f544de70446e1ee747854a97f4cf3b36ab7df
Chrisy <chrisyhaigh@gmail.com>
2023-12-20 23:17:36 +0000

    add functions for adding & removing shop list items to the UI and also adding and removing to the itemsArray

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-//Sets up Firebase Database
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-const appSettings = {
-    databaseURL: 'https://shoppingcart-a5a12-default-rtdb.europe-west1.firebasedatabase.app/'
-}
-
-const app = initializeApp(appSettings);
-const dataBase = getDatabase(app);
-const itemsInDB = ref(dataBase, "shopping");
-
-//
 
 let inputField = document.getElementById('input-field');
 let addList = document.getElementById('add-list-btn');
 let listItems = document.getElementById('list-items');
 
+let itemsArray = [];
+
 addList.addEventListener('click', function() {
     let inputValue = inputField.value;
 
-    push(itemsInDB, inputValue);
-    addToCart(inputValue);
-    clearInput();
+    if (inputValue.length > 0) {
+        addToCart(inputValue);
+        itemsArray.push(inputValue);
+        clearInput();
+        console.log('Current Items: ', itemsArray)
+    }
 })
 
 inputField.addEventListener('keydown', function(event) {
@@ -30,9 +21,19 @@ inputField.addEventListener('keydown', function(event) {
 
     if (event.key === 'Enter') {
         addToCart(inputValue);
+        itemsArray.push(inputValue);
         clearInput();
+        console.log('Current Items: ', itemsArray)
     }
+
 });
+
+listItems.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        removeItem(event.target);
+    }    
+})
+
 
 function clearInput() {
     inputField.value = '';
@@ -45,4 +46,12 @@ function addToCart(item) {
         listItems.appendChild(list);
     }
 }
+
+function removeItem(item) {
+    listItems.removeChild(item);
+    itemsArray.splice(itemsArray.indexOf(item.textContent), 1);
+    console.log('Current Items: ', itemsArray);
+}
+
+
 
